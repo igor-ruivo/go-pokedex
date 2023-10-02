008e2e2315046819a43038101e93cc88a1752bcf
igor-ruivo <igor.ruivo.97@gmail.com>
2023-10-02 23:55:29 +0100

    Improve image size and slider size

--- src/components/CircularSliderInput.tsx
+++ src/components/CircularSliderInput.tsx
@@ -32,7 +32,7 @@ const CircularSliderInput = ({handleColor, value, setValue}: ICircularSliderInpu
         // should follow the same formula as the corresponding image
         // calc(140px + 100 * calc(100vw - 500px) / (1920 - 500));
         //const calc = Math.min(80 + 140 + 100 * (windowSize - 500) / (1920 - 500), ((windowSize - 0) / 2));
-        const calc = (11 / 7) * Math.min(140 + 100 * (windowSize - 500) / (1920 - 500), (windowSize - 0) / 2 * 0.6);
+        const calc = Math.min(275, (11 / 7) * Math.min(140 + 100 * (windowSize - 500) / (1920 - 500), (windowSize - 0) / 2 * 0.6));
         console.log(calc);
         return calc; //subtracting padding
     }
