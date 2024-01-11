fa34ace19cac8bfef0fc65880cd672d08abf43c0
igor-ruivo <igor.ruivo.97@gmail.com>
2024-01-11 00:06:58 +0000

    Fix Raid filter for pokémon deletion

--- src/components/DeleteTrash.tsx
+++ src/components/DeleteTrash.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { IGamemasterPokemon } from "../DTOs/IGamemasterPokemon";
 import { useLanguage } from "../contexts/language-context";
 import { usePokemon } from "../contexts/pokemon-context";
@@ -10,6 +10,9 @@ import "./DeleteTrash.scss"
 import { IRankedPokemon } from "../DTOs/IRankedPokemon";
 import { ConfigKeys, readPersistentValue, writePersistentValue } from "../utils/persistent-configs-handler";
 import LoadingRenderer from "./LoadingRenderer";
+import { useRaidRanker } from "../contexts/raid-ranker-context";
+import { useMoves } from "../contexts/moves-context";
+import { PokemonTypes } from "../DTOs/PokemonTypes";
 
 const parsePersistentCachedNumberValue = (key: ConfigKeys, defaultValue: number) => {
     const cachedValue = readPersistentValue(key);
@@ -21,6 +24,7 @@ const parsePersistentCachedNumberValue = (key: ConfigKeys, defaultValue: number)
 
 const DeleteTrash = () => {
     const {gamemasterPokemon, fetchCompleted} = usePokemon();
+    const {moves, movesFetchCompleted} = useMoves();
     const {rankLists, pvpFetchCompleted} = usePvp();
     const {currentGameLanguage} = useLanguage();
     const [trashGreat, setTrashGreat] = useState(parsePersistentCachedNumberValue(ConfigKeys.TrashGreat, 100));
@@ -28,49 +32,20 @@ const DeleteTrash = () => {
     const [trashMaster, setTrashMaster] = useState(parsePersistentCachedNumberValue(ConfigKeys.TrashMaster, 200));
     const [trashRaid, setTrashRaid] = useState(parsePersistentCachedNumberValue(ConfigKeys.TrashRaid, 20));
     const [cp, setCP] = useState(parsePersistentCachedNumberValue(ConfigKeys.TrashCP, 2000));
+    const {raidDPS, raidRankerFetchCompleted, computeRaidRankerforTypes} = useRaidRanker();
+    const [isCalculating, setIsCalculating] = useState(false);
 
     const targetRef = useRef<HTMLTextAreaElement>(null);
-
-    useEffect(() => {
-        if (targetRef.current) {
-            targetRef.current.value = "";
-        }
-        writePersistentValue(ConfigKeys.TrashGreat, trashGreat.toString());
-    }, [trashGreat, targetRef]);
-
-    useEffect(() => {
-        if (targetRef.current) {
-            targetRef.current.value = "";
-        }
-        writePersistentValue(ConfigKeys.TrashUltra, trashUltra.toString());
-    }, [trashUltra, targetRef]);
-
-    useEffect(() => {
-        if (targetRef.current) {
-            targetRef.current.value = "";
-        }
-        writePersistentValue(ConfigKeys.TrashMaster, trashMaster.toString());
-    }, [trashMaster, targetRef]);
-
-    useEffect(() => {
-        if (targetRef.current) {
-            targetRef.current.value = "";
-        }
-        writePersistentValue(ConfigKeys.TrashRaid, trashRaid.toString());
-    }, [trashRaid, targetRef]);
-
-    useEffect(() => {
-        if (targetRef.current) {
-            targetRef.current.value = "";
-        }
-        writePersistentValue(ConfigKeys.TrashCP, cp.toString());
-    }, [cp, targetRef]);
-
-    const isBadRank = (rank: number, rankLimit: number) => {
+    
+    const enumValues: PokemonTypes[] = Object.keys(PokemonTypes)
+        .filter(key => isNaN(Number(key)) && key !== "Normal")
+        .map(key => key as unknown as PokemonTypes);
+    
+    const isBadRank = useCallback((rank: number, rankLimit: number) => {
         return rank === Infinity || rank > rankLimit;
-    }
+    }, []);
 
-    const needsLessThanFiveAttack = (p: IGamemasterPokemon, leagueIndex: number) => {
+    const needsLessThanFiveAttack = useCallback((p: IGamemasterPokemon, leagueIndex: number) => {
         const bestIVs = Object.values(computeBestIVs(p.atk, p.def, p.hp, leagueIndex === 0 ? 1500 : leagueIndex === 1 ? 2500 : Number.MAX_VALUE)).flat();
         for (let i = 0; i < 20; i++) {
             const neededAtk = bestIVs[i].IVs.A;
@@ -79,9 +54,42 @@ const DeleteTrash = () => {
             }
         }
         return true;
-    }
+    }, []);
 
-    const isBadWithAttack = (p: IGamemasterPokemon) => {
+    const isGoodForRaids = useCallback((p: IGamemasterPokemon) => {
+        if (!fetchCompleted || !raidRankerFetchCompleted(enumValues)) {
+            return false;
+        }
+
+        const reachablePokemon = Array.from(fetchReachablePokemonIncludingSelf(p, gamemasterPokemon));
+        const exceptionPokemon = ["slowpoke_galarian", "slowbro_galarian"];
+
+        const mega = (p.isShadow || exceptionPokemon.includes(p.speciesId)) ? [] : Object.values(gamemasterPokemon)
+            .filter(pk => !pk.aliasId && pk.isMega && reachablePokemon.some(r => r.dex === pk.dex));
+
+        let minRaidRank = Infinity;
+        const finalCollection = [...reachablePokemon, ...mega];
+        enumValues.forEach(t => {
+            finalCollection.forEach(pk => {
+                const rank = Object.keys(raidDPS[t.toString().toLocaleLowerCase()]).indexOf(pk.speciesId);
+                if (rank > -1) {
+                    minRaidRank = Math.min(minRaidRank, rank + 1);
+                }
+            });
+        });
+
+        return minRaidRank <= trashRaid;
+    }, [fetchCompleted, trashRaid, raidRankerFetchCompleted, enumValues, gamemasterPokemon, raidDPS]);
+
+    const isBadWithAttack = useCallback((p: IGamemasterPokemon) => {
+        if (!fetchCompleted || !pvpFetchCompleted) {
+            return false;
+        }
+
+        if (isGoodForRaids(p)) {
+            return false;
+        }
+
         // here, the pokémon is good for something...
         // find those good for gl and ul that need <5 atk in the first 20 spots, except if rank for great or ultra is <= 20
         const reachablePokemon = Array.from(fetchReachablePokemonIncludingSelf(p, gamemasterPokemon));
@@ -127,31 +135,26 @@ const DeleteTrash = () => {
         }
 
         return false;
-    }
+    }, [fetchCompleted, pvpFetchCompleted, isGoodForRaids, gamemasterPokemon, rankLists, trashMaster, trashGreat, trashUltra, isBadRank, needsLessThanFiveAttack]);
 
-    const isAlwaysBadPokemon = (p: IGamemasterPokemon) => {
+    const isAlwaysBadPokemon = useCallback((p: IGamemasterPokemon) => {
+        if (!fetchCompleted || !pvpFetchCompleted) {
+            return false;
+        }
+
         const reachablePokemon = Array.from(fetchReachablePokemonIncludingSelf(p, gamemasterPokemon));
         const glLowestRank = Math.min(...reachablePokemon.map(p => rankLists[0][p.speciesId]?.rank).filter(r => r));
         const ulLowestRank = Math.min(...reachablePokemon.map(p => rankLists[1][p.speciesId]?.rank).filter(r => r));
         const mlLowestRank = Math.min(...reachablePokemon.map(p => rankLists[2][p.speciesId]?.rank).filter(r => r));
         
-        return isBadRank(glLowestRank, trashGreat) && isBadRank(ulLowestRank, trashUltra) && isBadRank(mlLowestRank, trashMaster);
-    }
+        return !isGoodForRaids(p) && isBadRank(glLowestRank, trashGreat) && isBadRank(ulLowestRank, trashUltra) && isBadRank(mlLowestRank, trashMaster);
+    }, [fetchCompleted, isGoodForRaids, pvpFetchCompleted, gamemasterPokemon, rankLists, isBadRank, trashGreat, trashUltra, trashMaster]);
 
-    if (!fetchCompleted || !pvpFetchCompleted) {
-        return <span className="text-color">Fetching Pokémon...</span>;
-    }
+    const computeStr = useCallback(() => {
+        if (!fetchCompleted) {
+            return "";
+        }
 
-    type DisambiguatedEntry = {
-        dex: number,
-        shadow: boolean,
-        alolan: boolean,
-        galarian: boolean,
-        hisuian: boolean,
-        paldean: boolean
-    }
-
-    const computeStr = () => {
         const dexesWithGoodForms = new Set<number>();
         const potentiallyDeletablePokemon = new Set<number>();
         const pokemonBadWithAttack: Dictionary<DisambiguatedEntry[]> = {};
@@ -231,6 +234,84 @@ const DeleteTrash = () => {
 
         str += `&!4*&!#&!${gameTranslator(GameTranslatorKeys.Legendary, currentGameLanguage)}&!${gameTranslator(GameTranslatorKeys.Mythical, currentGameLanguage)}&!${gameTranslator(GameTranslatorKeys.CP, currentGameLanguage)}${cp}-&!${gameTranslator(GameTranslatorKeys.Favorite, currentGameLanguage)}&!${gameTranslator(GameTranslatorKeys.MegaEvolve, currentGameLanguage)}&!${gameTranslator(GameTranslatorKeys.UltraBeast, currentGameLanguage)}&!${gameTranslator(GameTranslatorKeys.TradeEvolve, currentGameLanguage)}`;
         return str;
+    }, [fetchCompleted, gamemasterPokemon, isAlwaysBadPokemon, isBadWithAttack, cp, currentGameLanguage]);
+
+    useEffect(() => {
+        if (!isCalculating) {
+            return;
+        }
+        
+        if (targetRef?.current) { 
+            targetRef.current.value = "Computing data...";
+        }
+
+        setTimeout(() => {
+            if (targetRef?.current) {
+                targetRef.current.value = computeStr();
+            }
+            setIsCalculating(false);
+        }, 100);
+        
+    }, [isCalculating, computeStr, targetRef]);
+
+    useEffect(() => {
+
+        if (!isCalculating || !fetchCompleted || !movesFetchCompleted || raidRankerFetchCompleted(enumValues)) {
+            return;
+        }
+
+        if (targetRef?.current) { 
+            targetRef.current.value = "Loading Pokémon...";
+        }
+
+        setTimeout(() => {
+            computeRaidRankerforTypes(gamemasterPokemon, moves, enumValues);
+        }, 100);
+
+    }, [fetchCompleted, movesFetchCompleted, enumValues, gamemasterPokemon, isCalculating, moves, raidRankerFetchCompleted, computeRaidRankerforTypes]);
+
+    useEffect(() => {
+        if (targetRef.current) {
+            targetRef.current.value = "";
+        }
+        writePersistentValue(ConfigKeys.TrashGreat, trashGreat.toString());
+    }, [trashGreat, targetRef]);
+
+    useEffect(() => {
+        if (targetRef.current) {
+            targetRef.current.value = "";
+        }
+        writePersistentValue(ConfigKeys.TrashUltra, trashUltra.toString());
+    }, [trashUltra, targetRef]);
+
+    useEffect(() => {
+        if (targetRef.current) {
+            targetRef.current.value = "";
+        }
+        writePersistentValue(ConfigKeys.TrashMaster, trashMaster.toString());
+    }, [trashMaster, targetRef]);
+
+    useEffect(() => {
+        if (targetRef.current) {
+            targetRef.current.value = "";
+        }
+        writePersistentValue(ConfigKeys.TrashRaid, trashRaid.toString());
+    }, [trashRaid, targetRef]);
+
+    useEffect(() => {
+        if (targetRef.current) {
+            targetRef.current.value = "";
+        }
+        writePersistentValue(ConfigKeys.TrashCP, cp.toString());
+    }, [cp, targetRef]);
+    
+    type DisambiguatedEntry = {
+        dex: number,
+        shadow: boolean,
+        alolan: boolean,
+        galarian: boolean,
+        hisuian: boolean,
+        paldean: boolean
     }
 
     return (
@@ -256,19 +337,19 @@ const DeleteTrash = () => {
                                 </div>
                                 <div className="row-flex">
                                 <div>
-                                <img height={20} width={20} src={`${process.env.PUBLIC_URL}/images/leagues/great.png`}/> Top <select value={trashGreat} onChange={e => setTrashGreat(+e.target.value)} className="select-level">
+                                <img height={20} width={20} alt="Great League" src={`${process.env.PUBLIC_URL}/images/leagues/great.png`}/> Top <select value={trashGreat} onChange={e => setTrashGreat(+e.target.value)} className="select-level">
                                         {Array.from({length: 2000}, (_x, i) => i)
                                             .map(e => (<option key={e} value={e}>{e}</option>))}
                                 </select>
                                 </div>
                                 <div>
-                                <img height={20} width={20} src={`${process.env.PUBLIC_URL}/images/leagues/ultra.png`}/> Top <select value={trashUltra} onChange={e => setTrashUltra(+e.target.value)} className="select-level">
+                                <img height={20} width={20} alt="Ultra League" src={`${process.env.PUBLIC_URL}/images/leagues/ultra.png`}/> Top <select value={trashUltra} onChange={e => setTrashUltra(+e.target.value)} className="select-level">
                                     {Array.from({length: 2000}, (_x, i) => i)
                                         .map(e => (<option key={e} value={e}>{e}</option>))}
                                 </select>
                                 </div>
                                 <div>
-                                <img height={20} width={20} src={`${process.env.PUBLIC_URL}/images/leagues/master.png`}/> Top <select value={trashMaster} onChange={e => setTrashMaster(+e.target.value)} className="select-level">
+                                <img height={20} width={20} alt="Master League" src={`${process.env.PUBLIC_URL}/images/leagues/master.png`}/> Top <select value={trashMaster} onChange={e => setTrashMaster(+e.target.value)} className="select-level">
                                     {Array.from({length: 2000}, (_x, i) => i)
                                         .map(e => (<option key={e} value={e}>{e}</option>))}
                                 </select>
@@ -283,15 +364,8 @@ const DeleteTrash = () => {
                                 </div>
                                 </div>
                             </div>
-                            <button className="dark-text main-btn with-big-top-margin" onClick={() => {
-                                if (targetRef?.current) {
-                                    targetRef.current.value = "Loading data...";
-                                    setTimeout(() => {
-                                        if (targetRef?.current) {
-                                            targetRef.current.value = computeStr();
-                                        }
-                                    }, 100);
-                                }
+                            <button className="dark-text main-btn with-big-top-margin" disabled={isCalculating} onClick={(e) => {
+                                setIsCalculating(true);
                             }}>Calculate!</button>
                             <textarea
                                 ref={targetRef}
