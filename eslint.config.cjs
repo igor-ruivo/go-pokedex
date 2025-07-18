3999137599e10638cab65fcf81bda0b26ec17029
Igor Ruivo <igor.ruivo.97@gmail.com>
2025-07-18 17:22:28 +0100

    Fix linter with promise and jsx-ally

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,11 +1,14 @@
 const reactPlugin = require('eslint-plugin-react');
 const hooksPlugin = require('eslint-plugin-react-hooks');
+const promisePlugin = require('eslint-plugin-promise');
+const jsxA11y = require('eslint-plugin-jsx-a11y');
 const { config } = require('typescript-eslint');
 
 const {
 	generateEslintConfigs,
 	getJsConfigs,
 	getTsConfigs,
+	configureWithPossibleExtension,
 } = require('@ti-platform/aide-build-tools');
 
 const baseDir = __dirname;
@@ -15,13 +18,40 @@ const configs = generateEslintConfigs({
 	enable: ['html', 'cjs', 'json', 'ts'],
 });
 
-const baseTsConfigs = getTsConfigs(getJsConfigs(), baseDir);
+const baseTsConfigs = configureWithPossibleExtension(
+	getTsConfigs(getJsConfigs(), baseDir),
+	(configs) => {
+		return {
+			...configs,
+			plugins: {
+				...configs.plugins,
+				'promise': promisePlugin,
+				'jsx-a11y': jsxA11y,
+			},
+			rules: {
+				...configs.rules,
+				...promisePlugin.configs.recommended.rules,
+				...jsxA11y.configs.recommended.rules,
+			},
+		};
+	}
+);
 
 if (baseTsConfigs) {
 	const tsxConfigWithExtends = {
 		...baseTsConfigs,
 		name: 'react-tsx additional ruleset',
 		files: ['**/*.tsx'],
+		languageOptions: {
+			...baseTsConfigs.languageOptions,
+			parserOptions: {
+				...baseTsConfigs.languageOptions?.parserOptions,
+				ecmaFeatures: {
+					...baseTsConfigs.languageOptions?.parserOptions?.ecmaFeatures,
+					jsx: true,
+				},
+			},
+		},
 		plugins: {
 			...baseTsConfigs.plugins,
 			'react': reactPlugin,
@@ -34,6 +64,7 @@ if (baseTsConfigs) {
 			'react/react-in-jsx-scope': 'off',
 		},
 		settings: {
+			...baseTsConfigs.settings,
 			'react': { version: 'detect' },
 			'import/resolver': {
 				typescript: {},
