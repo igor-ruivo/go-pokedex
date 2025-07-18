677952fe1a5b70b1e573f92b2731283351878f82
Igor Ruivo <igor.ruivo.97@gmail.com>
2025-07-18 04:31:43 +0100

    Add more ts rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,12 +29,42 @@ module.exports = {
 		'plugin:jsx-a11y/recommended',
 		'plugin:import/recommended',
 		'plugin:promise/recommended',
-		'prettier', // Using prettier from the CLI instead
+		'prettier',
 	],
 	rules: {
+		'no-case-declarations': 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'@typescript-eslint/array-type': ['error', { default: 'generic' }],
+    '@typescript-eslint/consistent-generic-constructors': ['error', 'constructor'],
+    '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+    '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' },
+    ],
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-exports': [
+        'error',
+        {
+            fixMixedExportsWithInlineTypeSpecifier: true,
+        },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+            prefer: 'type-imports',
+            fixStyle: 'separate-type-imports',
+        },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-redundant-type-constituents': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none', caughtErrorsIgnorePattern: '^ignore' }],
+    '@typescript-eslint/only-throw-error': 'off',
+    '@typescript-eslint/prefer-function-type': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
 		'react/react-in-jsx-scope': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
 		'no-console': 'warn',
 		'unused-imports/no-unused-imports': 'error',
 		'import/order': [
