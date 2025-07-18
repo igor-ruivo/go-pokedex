668079b7a9b7b9eeafff04c987abbfc0fd322680
Igor Ruivo <igor.ruivo.97@gmail.com>
2025-07-18 04:36:07 +0100

    Format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,42 +35,51 @@ module.exports = {
 		'no-case-declarations': 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'@typescript-eslint/array-type': ['error', { default: 'generic' }],
-    '@typescript-eslint/consistent-generic-constructors': ['error', 'constructor'],
-    '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
-    '@typescript-eslint/consistent-type-assertions': [
-        'error',
-        { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' },
-    ],
-    '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/consistent-type-exports': [
-        'error',
-        {
-            fixMixedExportsWithInlineTypeSpecifier: true,
-        },
-    ],
-    '@typescript-eslint/consistent-type-imports': [
-        'error',
-        {
-            prefer: 'type-imports',
-            fixStyle: 'separate-type-imports',
-        },
-    ],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-redundant-type-constituents': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { args: 'none', caughtErrorsIgnorePattern: '^ignore' }],
-    '@typescript-eslint/only-throw-error': 'off',
-    '@typescript-eslint/prefer-function-type': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
+		'@typescript-eslint/consistent-generic-constructors': [
+			'error',
+			'constructor',
+		],
+		'@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+		'@typescript-eslint/consistent-type-assertions': [
+			'error',
+			{
+				assertionStyle: 'as',
+				objectLiteralTypeAssertions: 'allow-as-parameter',
+			},
+		],
+		'@typescript-eslint/consistent-type-definitions': 'off',
+		'@typescript-eslint/consistent-type-exports': [
+			'error',
+			{
+				fixMixedExportsWithInlineTypeSpecifier: true,
+			},
+		],
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{
+				prefer: 'type-imports',
+				fixStyle: 'separate-type-imports',
+			},
+		],
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-redundant-type-constituents': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{ args: 'none', caughtErrorsIgnorePattern: '^ignore' },
+		],
+		'@typescript-eslint/only-throw-error': 'off',
+		'@typescript-eslint/prefer-function-type': 'off',
+		'@typescript-eslint/restrict-template-expressions': 'off',
+		'@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
 		'react/react-in-jsx-scope': 'off',
 		'no-console': 'warn',
 		'unused-imports/no-unused-imports': 'error',
 		'import/order': [
 			'error',
 			{
-				groups: [
+				'groups': [
 					'builtin',
 					'external',
 					'internal',
@@ -78,7 +87,7 @@ module.exports = {
 					'sibling',
 					'index',
 				],
-				alphabetize: { order: 'asc', caseInsensitive: true },
+				'alphabetize': { order: 'asc', caseInsensitive: true },
 				'newlines-between': 'always',
 			},
 		],
@@ -92,7 +101,7 @@ module.exports = {
 		],
 	},
 	settings: {
-		react: { version: 'detect' },
+		'react': { version: 'detect' },
 		'import/resolver': {
 			typescript: {},
 		},
