c6c85d962364575f567e014c279c36c6c8041403
Igor Ruivo <igor.ruivo.97@gmail.com>
2025-07-18 03:02:34 +0100

    Finish configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import './App.scss';
 
-const abra = async () => {
-	return {} as any;
-};
-
 const App = () => {
-	return (
-		<div>
-			<br></br>
-			<b></b>Hello World!
-		</div>
-	);
+	return <>Hello World!</>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,7 @@
 import './index.scss';
-
-import { useEffect, useState } from 'react';
-// unused import
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
-
-const A = () => {
-	const [mystate, thesater] = useState(0);
-	useEffect(() => {
-		if (mystate) thesater(mystate + 1);
-	}, []); // missing dependency
-	return <div></div>;
-};
-
-const List = () => {
-	return (
-		<ul>
-			{[1, 2, 3].map(i => (
-				<li>{i}</li>
-			))}{' '}
-			{/* missing key */}
-		</ul>
-	);
-};
